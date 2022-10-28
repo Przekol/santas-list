9b1f822645e3360f078ebd75a3028c74e1f83d23
Przemyslaw Wojciechowski <przemyslaw.r.wojciechowski@gmail.com>
2022-10-28 22:21:10 +0200

    Extract getGiftsAll method

--- src/gift/gift.controller.ts
+++ src/gift/gift.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { GiftsList } from '../types/gift';
+import { GiftService } from './gift.service';
 
 @Controller('gift')
-export class GiftController {}
+export class GiftController {
+  constructor(@Inject(GiftService) private giftService: GiftService) {}
+
+  @Get('/')
+  async getGiftsAll(): Promise<GiftsList> {
+    return this.giftService.getItems();
+  }
+}
--- src/gift/gift.service.ts
+++ src/gift/gift.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { GiftsList } from '../types/gift';
+import { Gift } from './entities/gift.entity';
 
 @Injectable()
-export class GiftService {}
+export class GiftService {
+  async getItems(): Promise<GiftsList> {
+    const giftsList = await Gift.find();
+    return { giftsList };
+  }
+}
