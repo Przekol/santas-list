220c5040d9e7d720ea47ba31c3f5698124dbbebf
Przemyslaw Wojciechowski <przemyslaw.r.wojciechowski@gmail.com>
2022-10-30 16:55:59 +0100

    Extract getOneChild method

--- src/child/child.controller.ts
+++ src/child/child.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get, Inject, Param } from '@nestjs/common';
 
 import { ChildService } from './child.service';
 import { Child } from './entities/child.entity';
+import { SingleChild } from '../types/child';
 
 @Controller('child')
 export class ChildController {
   constructor(@Inject(ChildService) private childService: ChildService) {}
+
   @Get('/')
   getAllChild(): Promise<Child[]> {
     return this.childService.getAllChild();
+  }
+
+  @Get('/:id')
+  getOneChild(@Param('id') id: string): Promise<SingleChild> {
+    return this.childService.getOneChild(id);
   }
 }
--- src/child/child.service.ts
+++ src/child/child.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { Child } from './entities/child.entity';
+import { SingleChild } from '../types/child';
 
 @Injectable()
 export class ChildService {
   async getAllChild(): Promise<Child[]> {
     return await Child.find({ relations: ['gift'] });
+  }
+
+  async getOneChild(id: string): Promise<SingleChild> {
+    const [child] = await Child.find({ where: { id }, relations: ['gift'] });
+    if (!child) {
+      throw new Error('Child not found.');
+    }
+    return child;
   }
 }
