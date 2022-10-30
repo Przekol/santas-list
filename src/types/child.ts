90c59d22a23d0531c3546b847c0af688c1aaef41
Przemyslaw Wojciechowski <przemyslaw.r.wojciechowski@gmail.com>
2022-10-30 16:55:36 +0100

    Add SingleChild type and change ChildInterface

--- src/types/child.ts
+++ src/types/child.ts
@@ -1,4 +1,8 @@
+import { GiftInterface } from './gift';
+
 export interface ChildInterface {
   id: string;
   name: string;
+  gift: GiftInterface | null;
 }
+export type SingleChild = ChildInterface;
