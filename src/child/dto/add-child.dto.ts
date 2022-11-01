08e373ecaa69fe53c96ef11221d792cf088dced4
Przemyslaw Wojciechowski <przemyslaw.r.wojciechowski@gmail.com>
2022-11-01 15:34:39 +0100

    Add validations DTO

--- src/child/dto/add-child.dto.ts
+++ src/child/dto/add-child.dto.ts
@@ -1,3 +1,9 @@
+import { IsString, Length } from 'class-validator';
+
 export class AddChildDto {
+  @IsString()
+  @Length(3, 25, {
+    message: 'Name must be between 3 and 25 characters long.',
+  })
   name: string;
 }
