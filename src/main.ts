a8c5232db720ee31d0552e585c417f0b81985dcb
Przemyslaw Wojciechowski <przemyslaw.r.wojciechowski@gmail.com>
2022-11-02 08:05:26 +0100

    Add ExceptionsLoggerFilter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ExceptionsLoggerFilter } from './utils/exceptions-logger.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new ExceptionsLoggerFilter(httpAdapter));
   await app.listen(3000);
 }
 bootstrap();
