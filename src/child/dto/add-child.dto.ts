0317aa618838f3512b88103160fe8dda5dbf36af
Przemyslaw Wojciechowski <przemyslaw.r.wojciechowski@gmail.com>
2022-11-07 10:13:45 +0100

    Add IsNotEmpty in add-child.dto.ts

--- src/child/dto/add-child.dto.ts
+++ src/child/dto/add-child.dto.ts
@@ -1,7 +1,8 @@
-import { IsString, Length } from 'class-validator';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class AddChildDto {
   @IsString()
+  @IsNotEmpty()
   @Length(3, 25, {
     message: 'Name must be between 3 and 25 characters long.',
   })
