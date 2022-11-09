146d000667575fe6b5473a4bb602d5546b1897af
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2022-11-09 17:56:03 +0100

    Change name class Dto

--- src/child/dto/add-gift-for-child.dto.ts
+++ src/child/dto/add-gift-for-child.dto.ts
@@ -2,7 +2,7 @@ import { IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { AddGiftForChildReq } from '../../types/child';
 
-export class AddGiftForChild implements AddGiftForChildReq {
+export class AddGiftForChildDto implements AddGiftForChildReq {
   @IsString()
   @ApiProperty({ format: 'uuid' })
   giftId: string;
