ea3500e1cc03d82cec3b9edf9145af2e0496b6b7
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2022-11-09 17:56:51 +0100

    Add Api swagger

--- src/child/child.controller.ts
+++ src/child/child.controller.ts
@@ -3,21 +3,32 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Inject,
   Param,
   Patch,
   Post,
 } from '@nestjs/common';
-
 import { ChildService } from './child.service';
 import { AddChildDto } from './dto/add-child.dto';
-import { GetSuccessInfo } from '../types/success-info';
-import { AddGiftForChild } from './dto/add-gift-for-child.dto';
-import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { AddGiftForChildDto } from './dto/add-gift-for-child.dto';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiNoContentResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { Child } from './entities/child.entity';
 import { GetListOfChildrenRes, GetOneChildRes } from '../types/child';
+import { ApiMessage } from 'src/utils/messages/api';
 
 @ApiTags('Child')
+@ApiInternalServerErrorResponse({
+  description: ApiMessage.INTERNAL_SERVER_ERROR,
+})
 @Controller('child')
 export class ChildController {
   constructor(
@@ -25,45 +36,55 @@ export class ChildController {
   ) {}
 
   @ApiOkResponse({
-    description:
-      'Children array results from child table with Gift table relation.',
+    description: ApiMessage.ARRAY_OF_ALL_RECORDS,
+    isArray: true,
   })
   @Get('/')
   getAllChild(): Promise<GetListOfChildrenRes> {
     return this.childService.getAllChild();
   }
 
-  @ApiOkResponse({ description: 'One child result' })
+  @ApiOkResponse({ description: ApiMessage.THE_ONE_RESUL })
+  @ApiBadRequestResponse({ description: ApiMessage.THE_RECORD_NOT_FOUND })
   @Get('/:id')
   getOneChild(@Param('id') id: string): Promise<GetOneChildRes> {
     return this.childService.getOneChild(id);
   }
 
   @ApiCreatedResponse({
-    description: 'The child has been successfully created.',
+    description: ApiMessage.THE_RECORD_BEEN_SUCCESSFULLY_CREATED,
     type: Child,
   })
+  @ApiBody({ type: AddChildDto })
+  @ApiBadRequestResponse({ description: ApiMessage.INVALID_INPUT })
   @Post('/')
-  addNewChild(@Body() req: AddChildDto): Promise<GetOneChildRes> {
-    return this.childService.addNewChild(req);
+  addNewChild(@Body() dto: AddChildDto): Promise<GetOneChildRes> {
+    return this.childService.addNewChild(dto);
   }
 
-  @ApiCreatedResponse({
+  @ApiOkResponse({
     description: 'The gift has been successfully assigned to the child.',
+    type: Child,
+  })
+  @ApiBody({ type: AddGiftForChildDto })
+  @ApiBadRequestResponse({
+    description: ApiMessage.THE_RECORD_NOT_FOUND,
   })
   @Patch('/gift/:childId')
   addGiftForChild(
     @Param('childId') childId: string,
-    @Body() req: AddGiftForChild,
+    @Body() dto: AddGiftForChildDto,
   ): Promise<GetOneChildRes> {
-    return this.childService.addGiftForChild(childId, req);
+    return this.childService.addGiftForChild(childId, dto);
   }
 
-  @ApiOkResponse({
-    description: 'The child deleted.',
+  @ApiNoContentResponse({
+    description: ApiMessage.THE_RECORD_DELETED,
   })
+  @ApiBadRequestResponse({ description: ApiMessage.THE_RECORD_NOT_FOUND })
+  @HttpCode(204)
   @Delete('/:id')
-  deleteChild(@Param('id') id: string): Promise<GetSuccessInfo> {
+  deleteChild(@Param('id') id: string): Promise<void> {
     return this.childService.deleteChild(id);
   }
 }
--- src/gift/gift.controller.ts
+++ src/gift/gift.controller.ts
@@ -3,52 +3,74 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Inject,
   Param,
   Post,
 } from '@nestjs/common';
 import { GiftService } from './gift.service';
-import { GetSuccessInfo } from '../types/success-info';
 import { AddGiftDto } from './dto/add-gift.dto';
-import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiMethodNotAllowedResponse,
+  ApiNoContentResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { Gift } from './entities/gift.entity';
 import {
   GetListOfGiftsRes,
   GetOneGiftRes,
   GetSingleGiftResponse,
 } from '../types/gift';
+import { ApiMessage } from '../utils/messages/api';
 
 @ApiTags('Gift')
+@ApiInternalServerErrorResponse({
+  description: ApiMessage.INTERNAL_SERVER_ERROR,
+})
 @Controller('gift')
 export class GiftController {
   constructor(@Inject(GiftService) private readonly giftService: GiftService) {}
 
   @ApiOkResponse({
-    description: 'Gifts array results from gift table.',
+    description: ApiMessage.ARRAY_OF_ALL_RECORDS,
+    isArray: true,
   })
   @Get('/')
   getAllGifts(): Promise<GetListOfGiftsRes> {
     return this.giftService.getAllGifts();
   }
 
-  @ApiOkResponse({ description: 'One gift result' })
+  @ApiOkResponse({ description: ApiMessage.THE_ONE_RESUL })
+  @ApiBadRequestResponse({ description: ApiMessage.THE_RECORD_NOT_FOUND })
   @Get('/:id')
   getOneGift(@Param('id') id: string): Promise<GetSingleGiftResponse> {
     return this.giftService.getOneGift(id);
   }
 
-  @ApiOkResponse({
-    description: 'The gift deleted.',
+  @ApiNoContentResponse({
+    description: ApiMessage.THE_RECORD_DELETED,
   })
+  @ApiBadRequestResponse({ description: ApiMessage.THE_RECORD_NOT_FOUND })
+  @ApiMethodNotAllowedResponse({
+    description: ApiMessage.CANNOT_DELETE_GIVEN_RECORD,
+  })
+  @HttpCode(204)
   @Delete('/:id')
-  deleteGift(@Param('id') id: string): Promise<GetSuccessInfo> {
+  deleteGift(@Param('id') id: string): Promise<void> {
     return this.giftService.deleteGift(id);
   }
 
   @ApiCreatedResponse({
-    description: 'The child has been successfully created.',
+    description: ApiMessage.THE_RECORD_BEEN_SUCCESSFULLY_CREATED,
     type: Gift,
   })
+  @ApiBody({ type: AddGiftDto })
+  @ApiBadRequestResponse({ description: ApiMessage.INVALID_INPUT })
   @Post('/')
   addNewGift(@Body() req: AddGiftDto): Promise<GetOneGiftRes> {
     return this.giftService.addNewGift(req);
